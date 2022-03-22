aa1fa9277bcba79c9ad769bf834b5408ef0e2835
fajarkur219 <fajarkur219@student.amikom.ac.id>
2022-03-22 14:04:45 +0700

    Aplikasi_Taxi

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTaxi_21._11._4383
+{
+    public class Taxi
+    {
+        public string Drivername { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drivername: {0}", Drivername);
+            Console.WriteLine("OnDuty: {0}", OnDuty);
+            Console.WriteLine("NumPassenger: {0}", NumPassenger);
+    
+        }
+
+        public void PickUpPassenger() 
+        {
+            Console.WriteLine("{0} sedang Menjemput penumpang\n", Drivername);
+        }
+        public void DropOffPasssenger()
+        {
+            Console.WriteLine("{0} selesai Mengantar penumpang\n", Drivername);
+        }
+    }
+}
